c8614fdf53efa2d70a0ea87d74460630f69d56df
Ian Vlahovic <banddorkfht7@gmail.com>
2017-12-03 05:55:32 +0000

    Done.

--- lib/rescue_princess_peach.rb
+++ lib/rescue_princess_peach.rb
@@ -8,9 +8,5 @@ end
 
 ## Comment the below back in to see method scope in action!
 
-# rescue_princess_peach
-# puts status
-
-
-
-
+# rescue_princess_peach [This command doesn't display anything ("nil")]
+# puts status (This variable doesn't exist outside of #rescue_princess_peach; calling it will result in an error)
